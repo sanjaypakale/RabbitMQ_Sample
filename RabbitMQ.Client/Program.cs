4ea5519af2fdd83ecb369a0442d23410935658ae
sanjay pakale <sanjaypakale@gmail.com>
2020-02-29 18:08:09 +0530

    Add project files.

---
+++ RabbitMQ.Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Receive();
+        }
+
+        public static void Receive()
+        {
+            Console.WriteLine("starting consumption");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "niftyqueue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var deserialized = JsonConvert.DeserializeObject<AddUser>(message);
+                    Console.WriteLine("Creating user {0} {1}", deserialized.FirstName, deserialized.LastName);
+                };
+                channel.BasicConsume(queue: "niftyqueue", consumer: consumer);
+
+                Console.WriteLine("Done.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class AddUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ RabbitMQ.Server/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Send("sanjay", "pakale", "sanjaypakale@gmail.com");
+
+            Console.ReadLine();
+        }
+
+        public static void Send(string firstName, string lastName, string emailAddress)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "niftyqueue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                var command = new AddUser
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    EmailAddress = emailAddress,
+                    Password = "examplePassword"
+                };
+                string message = JsonConvert.SerializeObject(command);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                        routingKey: "niftyqueue",
+                                        basicProperties: null,
+                                        body: body);
+            }
+        }
+    }
+
+    public class AddUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
